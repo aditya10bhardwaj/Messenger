a0e25636f9295dfe561894670e931d28dd110fed
aditya10bhardwaj <138166827+aditya10bhardwaj@users.noreply.github.com>
2025-06-26 19:28:10 +0530

    Initial Commit

---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Aditya Bhardwaj on 26/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
